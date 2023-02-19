29654faedb78097aa9f620c241878d168e12f0ac
Everett Butler <everettjamesbutler@gmail.com>
2023-02-19 17:30:47 -0600

    got a chunk done of the submit function working to add information to the DOM!

--- client.js
+++ client.js
@@ -4,5 +4,27 @@ console.log('Yo waddup');
 function submitForm(event) {
     console.log(submitForm);
     event.preventDefault();
-    
+
+let firstName = document.querySelector('#firstName-input').value;
+
+let lastName = document.querySelector('#lastName-input').value;
+
+let idNumber = document.querySelector('#idNumber-input').value;
+
+let jobTitle = document.querySelector('#jobTitle-input').value;
+
+let annualSalary = document.querySelector('#salary-input').value;
+
+let employeeInfoTable = document.querySelector('#employee-information-output')
+employeeInfoTable.innerHTML += `
+    <tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${idNumber}</td>
+        <td>${jobTitle}</td>
+        <td>${annualSalary}</td>
+    </tr>
+
+`;
+
 }
